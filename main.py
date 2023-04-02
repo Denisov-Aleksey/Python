898908bb85b5a58d4f1cbe898190bee296179101
Denisov-Aleksey <work-denisov@mail.ru>
2023-04-02 22:54:54 +1200

    load

--- main.py
+++ main.py
@@ -12,5 +12,71 @@
 # number_of_apples = appels // schoolboy
 # print(number_of_apples)
 
+# schoolboy = int(input())
+# appels = int(input())
+# number_of_apples = appels % schoolboy
+# print(number_of_apples)
 
+# x = int(input())
+# y = x % 10
+# print(y)
+
+# x = int(input())
+# y = x // 10 % 10
+# print(y)
+
+# x = int(input())
+# print((x // 100) + (x // 10 % 10) + (x % 10))
+
+# from math import ceil
+#
+# n = int(input())
+# m = int(input())
+# print(ceil(m / n))
+
+
+# a = int(input())  # рубли
+# b = int(input())  # копейки
+# n = int(input())  # пирожки
+# coast = (a * 100 + b) * n
+# print(coast // 100, coast % 100)
+
+# следующие чётное
+# a = int(input())
+# print((a + 2) - (a % 2))
+
+# Проверка на положительность
+# x = int(input())
+# print (x >= 0)
+
+# Проверка на чётность
+# x = int(input())
+# print (x % 2 == 0)
+
+# # Проверка на кратность 6
+# x = int(input())
+# print (x % 6 == 0)
+
+# число не крано 9
+# x = int(input())
+# print (x % 9 > 0)
+
+# # Последняя цифра числа 2 ?
+# x = int(input())
+# print (x % 10 == 2)
+
+# # Оба числа деляться на 7
+# x = int(input())
+# y = int(input())
+# print(x % 7 == 0 and y % 7 == 0)
+
+# #Правильные треугольник
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print (a == b and b ==c and a ==c)
+
+# число двузначеное
+x = int(input())
+print(x >= 10 and x <= 99)
 
